0f87e918137d62f62135b99e10ec73e815601610
sadnessOjisan <sadness.ojisan@gmail.com>
2021-03-15 21:47:05 +0900

    can setup html parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use pulldown_cmark::{html, Parser};
+
 fn main() {
-    println!("Hello, world!");
+    let markdown_str = r#"# Hello
+人間は愚かな生物。
+
+[俺のブログ](https://blog.himanoa.net)
+"#;
+    let parser = Parser::new(markdown_str);
+    let mut html_buf = String::new();
+    html::push_html(&mut html_buf, parser);
+    println!("{}", html_buf);
 }
