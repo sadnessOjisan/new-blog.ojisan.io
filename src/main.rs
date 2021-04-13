35cbfb50a36360c954b5d2ddfe30b316b7ac24cb
sadness_ojisan <sadness.ojisan@gmail.com>
2021-04-14 01:10:36 +0900

    input content

--- src/main.rs
+++ src/main.rs
@@ -4,53 +4,54 @@ use std::{
     fs::{File, OpenOptions},
     path::Path,
 };
-use tera::{Tera,Context};
-
+use tera::{Context, Tera};
 
 fn main() {
-//     let markdown_str = r#"# Hello
-// 人間は愚かな生物。
+    let tera = match Tera::new("src/templates/*.html") {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Parsing error(s): {}", e);
+            ::std::process::exit(1);
+        }
+    };
+    let mut context = Context::new();
 
-// [俺のブログ](https://blog.himanoa.net)
-// "#;
-//     let parser = Parser::new(markdown_str);
-//     let mut html_buf = String::new();
-//     html::push_html(&mut html_buf, parser);
-//     println!("{}", html_buf);
+    let markdown_str = r#"# Hello
+人間は愚かな生物。
 
-//     // 実行位置からの相対パス
-//     let dir = fs::read_dir("./src/contents");
+[俺のブログ](https://blog.himanoa.net)
+"#;
+    let parser = Parser::new(markdown_str);
+    let mut html_buf = String::new();
+    html::push_html(&mut html_buf, parser);
+    println!("{}", html_buf);
 
-//     // https://doc.rust-jp.rs/rust-by-example-ja/std_misc/fs.html
-//     match dir {
-//         Err(why) => println!("! {:?}", why.kind()),
-//         Ok(paths) => {
-//             for path in paths {
-//                 let p = path.unwrap().path();
-//                 let s = p.to_str().unwrap();
-//                 let article_path = s.to_string() + "/index.md";
-//                 let a_p = Path::new(&article_path);
-//                 let mut f = File::open(a_p).unwrap();
-//                 let mut s = String::new();
-//                 f.read_to_string(&mut s);
-//                 let parser = Parser::new(&s);
-//                 let mut html_buf = String::new();
-//                 html::push_html(&mut html_buf, parser);
-//                 println!("{}", html_buf);
-//             }
-//         }
-//     }
+    // 実行位置からの相対パス
+    let dir = fs::read_dir("./src/contents");
+
+    // https://doc.rust-jp.rs/rust-by-example-ja/std_misc/fs.html
+    match dir {
+        Err(why) => println!("! {:?}", why.kind()),
+        Ok(paths) => {
+            for path in paths {
+                let p = path.unwrap().path();
+                let s = p.to_str().unwrap();
+                let article_path = s.to_string() + "/index.md";
+                let a_p = Path::new(&article_path);
+                let mut f = File::open(a_p).unwrap();
+                let mut s = String::new();
+                f.read_to_string(&mut s);
+                let parser = Parser::new(&s);
+                let mut html_buf = String::new();
+                html::push_html(&mut html_buf, parser);
+                context.insert("content", &html_buf);
+                let rendered = tera.render("post.html", &context);
+                println!("{:?}", rendered);
+            }
+        }
+    }
 
     // format!("{:?}", &dir);
 
-    let tera = match Tera::new("src/templates/*.html") {
-    Ok(t) => t,
-    Err(e) => {
-        println!("Parsing error(s): {}", e);
-        ::std::process::exit(1);
-    }
-};
-
-let rendered = tera.render("post.html", &Context::new());
-    println!("{:?}", rendered);
+    
 }
